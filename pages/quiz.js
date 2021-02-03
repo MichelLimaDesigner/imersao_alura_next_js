d8ec637da35ba03046d18412375e5a28b825d4e7
michellimacelebrar <michel@celebrar.co>
2021-02-02 23:03:27 -0300

    Created component to change quiz form layout and created a endpoint

--- pages/quiz.js
+++ pages/quiz.js
@@ -9,6 +9,7 @@ import GitHubCorner from '../src/components/GitHubCorner';
 // import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 import QuizContainer from '../src/components/QuizContainer';
+import AlternativesForm from '../src/components/AlternativesForm';
 
 function LoadingWidget() {
   return (
@@ -23,14 +24,43 @@ function LoadingWidget() {
   );
 }
 
+function ResultWidget({ results }) {
+  return (
+    <Widget>
+      <Widget.Header>
+        <h1> Resultado </h1>
+      </Widget.Header>
+      <Widget.Content>
+        <p>
+          {`Você acertou ${results.filter((result) => result).length} questões`}
+        </p>
+        <ul>
+          { results.map((result, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={`result__${index}`}>
+              { ` #${index + 1} - ${result ? 'Acertou' : 'Errou'} `}
+            </li>
+          )) }
+        </ul>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
 function QuestionWidget({
   questions,
   userName,
   totalQuestions,
   currentQuestion,
   onSubmit,
+  addResult,
 }) {
   const questionId = `question__${currentQuestion}`;
+  const [selectedAlternative, setSelectedAlternative] = useState(undefined);
+  const isCorrect = selectedAlternative === questions.answer;
+  const [formSubmited, setFormSubmited] = useState(false);
+  const hasSelectedAltenative = selectedAlternative !== undefined;
+
   return (
     <Widget>
       <Widget.Header>
@@ -56,25 +86,48 @@ function QuestionWidget({
           { questions.description }
         </p>
 
-        <form onSubmit={(event) => {
+        <AlternativesForm onSubmit={(event) => {
           event.preventDefault();
-          onSubmit();
+          setFormSubmited(true);
+          setTimeout(() => {
+            onSubmit();
+            addResult(isCorrect);
+            setFormSubmited(false);
+            setSelectedAlternative(undefined);
+          }, 3000);
         }}
         >
           { questions.alternatives.map((alternative, index) => {
             const alternativeId = `alternative__${index}`;
+            const alternativeStatus = isCorrect ? 'SUCESS' : 'ERROR';
+            const isSelected = selectedAlternative === index;
             return (
-              <Widget.Topic as="label" htmlFor={alternativeId} key={alternativeId}>
-                <input id={alternativeId} type="radio" name={questionId} />
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+                key={alternativeId}
+                data-selected={isSelected}
+                data-status={formSubmited && alternativeStatus}
+              >
+                <input
+                  style={{ display: 'none' }}
+                  id={alternativeId}
+                  type="radio"
+                  name={questionId}
+                  onChange={() => setSelectedAlternative(index)}
+                />
                 {alternative}
               </Widget.Topic>
             );
           }) }
 
-          <Button type="submit">
+          <Button type="submit" disabled={!hasSelectedAltenative}>
             Enviar
           </Button>
-        </form>
+
+          { formSubmited && isCorrect && <h3> Você acertou! </h3> }
+          { formSubmited && !isCorrect && <h3> Você errou! </h3> }
+        </AlternativesForm>
 
       </Widget.Content>
     </Widget>
@@ -93,6 +146,7 @@ function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const questions = db.questions[currentQuestion];
   const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const url = window.location.href;
@@ -106,11 +160,19 @@ function Quiz() {
     }, 1000);
   }, []);
 
+  function addResult(result) {
+    setResults([...results, result]);
+  }
+
   function handleSubmit() {
     const next = currentQuestion + 1 < totalQuestions;
 
     if (next) {
       setCurrentQuestion(currentQuestion + 1);
+      const radioChecked = Array.from(document.querySelectorAll("input[type='radio']")).find((input) => input.checked);
+
+      // eslint-disable-next-line no-unused-expressions
+      radioChecked ? radioChecked.checked = false : '';
     } else {
       setScreenState(screenStates.RESULT);
     }
@@ -130,6 +192,7 @@ function Quiz() {
           totalQuestions={totalQuestions}
           currentQuestion={currentQuestion}
           onSubmit={handleSubmit}
+          addResult={addResult}
         />
 
         )}
@@ -142,7 +205,7 @@ function Quiz() {
 
         { screenState === screenStates.RESULT && (
 
-        <h1> PA RA BÉNS </h1>
+        <ResultWidget results={results} />
 
         )}
 
---
+++ pages/quiz/[id].js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+function QuizDaGaleraPage() {
+  return (
+    <div>
+      Quiz da galera aqui
+    </div>
+  );
+}
+
+export default QuizDaGaleraPage;
